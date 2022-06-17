4ad07ccba205e52ac0b7f028ac6c7f8214161eca
thaisung <vuthaind@gmail.com>
2022-06-17 16:40:16 +0700

    fff

--- loginFB/urls.py
+++ loginFB/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+
 
 
 urlpatterns = [
