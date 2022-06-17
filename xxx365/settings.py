9af0b69ba5cf82757115f69cc5b1d0eede2be8ed
thaisung <vuthaind@gmail.com>
2022-06-17 14:06:58 +0700

    ul7

--- xxx365/settings.py
+++ xxx365/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'loginFB.urls'
+ROOT_URLCONF = 'xxx365.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'loginFB.wsgi.application'
+WSGI_APPLICATION = 'xxx365.wsgi.application'
 
 
 # Database
